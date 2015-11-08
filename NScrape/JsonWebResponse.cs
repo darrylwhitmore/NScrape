69bb2d8a14c9293826f2f924939e44e1bd00bdf1
Darryl Whitmore <darryl.whitmore@gmail.com>
2015-11-08 12:20:55 -0600

    Added XML documentation

--- NScrape/JsonWebResponse.cs
+++ NScrape/JsonWebResponse.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Text;
 
-namespace NScrape
-{
-    public class JsonWebResponse : TextWebResponse
-    {
-        internal JsonWebResponse(bool success, Uri url, string text, Encoding encoding)
-            : base(url, WebResponseType.JavaScript, success, text, encoding)
-        {
-        }
+namespace NScrape {
+	/// <summary>
+	/// Represents a web response for a request that returned JSON.
+	/// </summary>
+	public class JsonWebResponse : TextWebResponse {
 
-        /// <summary>
-        /// Gets the JSON data.
-        /// </summary>
-        public string Json { get { return Text; } }
-    }
+		internal JsonWebResponse( bool success, Uri url, string text, Encoding encoding )
+			: base( url, WebResponseType.JavaScript, success, text, encoding ) {
+		}
+
+		/// <summary>
+		/// Gets the JSON data.
+		/// </summary>
+		public string Json { get { return Text; } }
+	}
 }
