8d51628acec4dcb63a8e1d2249406d87adad4c0f
Darryl Whitmore <darryl.whitmore@gmail.com>
2015-11-11 20:28:01 -0600

    Revert "Added XML documentation"
    This reverts commit 69bb2d8a14c9293826f2f924939e44e1bd00bdf1.

--- NScrape/JsonWebResponse.cs
+++ NScrape/JsonWebResponse.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Text;
 
-namespace NScrape {
-	/// <summary>
-	/// Represents a web response for a request that returned JSON.
-	/// </summary>
-	public class JsonWebResponse : TextWebResponse {
+namespace NScrape
+{
+    public class JsonWebResponse : TextWebResponse
+    {
+        internal JsonWebResponse(bool success, Uri url, string text, Encoding encoding)
+            : base(url, WebResponseType.JavaScript, success, text, encoding)
+        {
+        }
 
-		internal JsonWebResponse( bool success, Uri url, string text, Encoding encoding )
-			: base( url, WebResponseType.JavaScript, success, text, encoding ) {
-		}
-
-		/// <summary>
-		/// Gets the JSON data.
-		/// </summary>
-		public string Json { get { return Text; } }
-	}
+        /// <summary>
+        /// Gets the JSON data.
+        /// </summary>
+        public string Json { get { return Text; } }
+    }
 }
