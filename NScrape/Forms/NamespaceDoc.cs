88d9d78878d06adec804244af663d11843b34446
Darryl Whitmore <darryl.whitmore@gmail.com>
2018-05-21 21:10:38 -0500

    Get basic helpfile generation working again

--- NScrape/Forms/NamespaceDoc.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NScrape.Forms {
-	/// <summary>
-	/// The <b>NScrape.Forms</b> namespace contains the types that provide HTML form handling functionality. 
-	/// </summary>
-	internal class NamespaceDoc {
-		// This class is used to define namespace comments for Sandcastle Help File Builder
-		private NamespaceDoc() { }
-	}
-}
