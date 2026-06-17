dd98c7eda21e82084be0e7bb3661e1d99f9b39eb
Darryl Whitmore <darryl.whitmore@gmail.com>
2026-06-17 18:38:02 -0500

    Refactor WebResponseFactory to support dependency injection and extensibility
    Replaced the static WebResponseFactory with an instance-based implementation
    that accepts an optional collection of IContentTypeHandler implementations at
    construction time. Custom handlers override built-in defaults, allowing callers
    to extend or replace content type handling without subclassing.
    
    Introduced IWebResponseFactory and IContentTypeHandler interfaces, an abstract
    ContentTypeHandlerBase, and concrete handler classes for all supported content
    types. Expanded built-in content type coverage to include common binary formats
    (zip, gzip, Office documents), feed types (RSS, Atom), JSON-LD, XHTML, and
    audio/video/font prefix matching.
    
    Updated WebClient to accept IWebResponseFactory via constructor injection,
    defaulting to a standard WebResponseFactory when none is provided.

---
+++ NScrape/ContentTypeHandlers/JavaScriptContentTypeHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using NScrape.Interfaces;
+using NScrape.Responses;
+
+namespace NScrape.ContentTypeHandlers;
+
+/// <summary>
+/// Handles HTTP responses with JavaScript content types.
+/// </summary>
+/// <remarks>
+/// This class is responsible for processing HTTP responses where the content type matches
+/// a JavaScript-related prefix (e.g., "application/javascript"). It creates instances of
+/// <see cref="NScrape.Responses.JavaScriptWebResponse"/> to represent the response data.
+/// </remarks>
+public class JavaScriptContentTypeHandler : ContentTypeHandlerBase {
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="NScrape.ContentTypeHandlers.JavaScriptContentTypeHandler"/> class.
+	/// </summary>
+	/// <param name="contentTypePrefix">
+	/// The prefix of the JavaScript-related content type that this handler will process.
+	/// </param>
+	/// <remarks>
+	/// This constructor specifies the content type prefix for JavaScript-related HTTP responses,
+	/// enabling the handler to identify and process such responses.
+	/// </remarks>
+	public JavaScriptContentTypeHandler( string contentTypePrefix ) : base( contentTypePrefix ) {
+	}
+	
+	/// <summary>
+	/// Creates a new instance of <see cref="NScrape.Responses.JavaScriptWebResponse"/> based on the provided HTTP response.
+	/// </summary>
+	/// <param name="httpWebResponse">The HTTP response to process.</param>
+	/// <returns>
+	/// An instance of <see cref="NScrape.Responses.JavaScriptWebResponse"/> representing the processed response.
+	/// </returns>
+	/// <remarks>
+	/// This method is overridden to handle HTTP responses with JavaScript-related content types.
+	/// It ensures that the response is encapsulated in a <see cref="NScrape.Responses.JavaScriptWebResponse"/> object.
+	/// </remarks>
+	public override IWebResponse CreateResponse( HttpWebResponse httpWebResponse ) => new JavaScriptWebResponse( true, httpWebResponse );
+}
--- NScrape/WebResponseFactory.cs
+++ NScrape/WebResponseFactory.cs
@@ -2,154 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using NScrape.ContentTypeHandlers;
 using NScrape.Interfaces;
 using NScrape.Responses;
 
 namespace NScrape {
+	
 	/// <summary>
-	/// Creates a <see cref="IWebResponse"/> object based on an <see cref="HttpWebResponse"/> object.
+	/// Represents a factory for creating instances of <see cref="NScrape.Interfaces.IWebResponse"/> 
+	/// based on the provided <see cref="System.Net.HttpWebResponse"/>.
 	/// </summary>
-	public class WebResponseFactory {
-		/// <summary>
-		/// Initializes static members of the <see cref="WebResponseFactory"/> class.
-		/// </summary>
-		static WebResponseFactory() {
-			SupportedContentTypes = new Dictionary<string, Func<HttpWebResponse, IWebResponse>> {
-				{ "application/atom+xml", CreateXmlResponse },
-				{ "application/gzip", CreateBinaryResponse },
-				{ "application/javascript", CreateJavaScriptResponse },
-				{ "application/json", CreateJsonResponse },
-				{ "application/ld+json", CreateJsonResponse },
-				{ "application/msword", CreateBinaryResponse },
-				{ "application/octet-stream", CreateBinaryResponse },
-				{ "application/pdf", CreateBinaryResponse },
-				{ "application/rss+xml", CreateXmlResponse },
-				{ "application/vnd.ms-excel", CreateBinaryResponse },
-				{ "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", CreateBinaryResponse },
-				{ "application/vnd.openxmlformats-officedocument.wordprocessingml.document", CreateBinaryResponse },
-				{ "application/x-dosexec", CreateBinaryResponse },
-				{ "application/x-javascript", CreateJavaScriptResponse },
-				{ "application/x-msdos-program", CreateBinaryResponse },
-				{ "application/x-zip-compressed", CreateBinaryResponse },
-				{ "application/xhtml+xml", CreateHtmlResponse },
-				{ "application/xml", CreateXmlResponse },
-				{ "application/zip", CreateBinaryResponse },
-				{ "audio/", CreateBinaryResponse },
-				{ "font/", CreateBinaryResponse },
-				{ "image/", CreateBinaryResponse },
-				{ "text/html", CreateHtmlResponse },
-				{ "text/javascript", CreateJavaScriptResponse },
-				{ "text/plain", CreatePlainTextResponse },
-				{ "text/xml", CreateXmlResponse },
-				{ "video/", CreateBinaryResponse },
-			};
-		}
+	/// <remarks>
+	/// This class allows customization of response creation by supporting a collection of 
+	/// <see cref="NScrape.Interfaces.IContentTypeHandler"/> implementations. These handlers 
+	/// can be used to process specific content types.
+	/// </remarks>
+	public class WebResponseFactory : IWebResponseFactory {
+		private static readonly IEnumerable<IContentTypeHandler> defaultHandlers = [
+			new BinaryContentTypeHandler( "application/gzip" ),
+			new BinaryContentTypeHandler( "application/msword" ),
+			new BinaryContentTypeHandler( "application/octet-stream" ),
+			new BinaryContentTypeHandler( "application/pdf" ),
+			new BinaryContentTypeHandler( "application/vnd.ms-excel" ),
+			new BinaryContentTypeHandler( "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ),
+			new BinaryContentTypeHandler( "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ),
+			new BinaryContentTypeHandler( "application/x-dosexec" ),
+			new BinaryContentTypeHandler( "application/x-msdos-program" ),
+			new BinaryContentTypeHandler( "application/x-zip-compressed" ),
+			new BinaryContentTypeHandler( "application/zip" ),
+			new BinaryContentTypeHandler( "audio/" ),
+			new BinaryContentTypeHandler( "font/" ),
+			new BinaryContentTypeHandler( "image/" ),
+			new BinaryContentTypeHandler( "video/" ),
+
+			new HtmlContentTypeHandler( "application/xhtml+xml" ),
+			new HtmlContentTypeHandler( "text/html" ),
+
+			new JavaScriptContentTypeHandler( "application/javascript" ),
+			new JavaScriptContentTypeHandler( "application/x-javascript" ),
+			new JavaScriptContentTypeHandler( "text/javascript" ),
+
+			new JsonContentTypeHandler( "application/json" ),
+			new JsonContentTypeHandler( "application/ld+json" ),
+			
+			new PlainTextContentTypeHandler( "text/plain" ),
+
+			new XmlContentTypeHandler( "application/atom+xml" ),
+			new XmlContentTypeHandler( "application/rss+xml" ),
+			new XmlContentTypeHandler( "application/xml" ),
+			new XmlContentTypeHandler( "text/xml" )
+		];
+
+		private readonly Dictionary<string, IContentTypeHandler> handlers;
 
 		/// <summary>
-		/// Gets a dictionary containing all content types currently supported by the
-		/// <see cref="WebResponseFactory"/>.
+		/// Initializes a new instance of the <see cref="NScrape.WebResponseFactory"/> class with default settings.
 		/// </summary>
 		/// <remarks>
-		/// The key of the dictionary is the text the content type
-		/// must start with (but not necessarily the full text of the content type).
-		/// <br/><br/>
-		/// The value is a
-		/// <see cref="Func{HttpWebResponse, IWebResponse}"/> that takes an <see cref="HttpWebResponse"/>
-		/// object with the given content type and returns a <see cref="IWebResponse"/>. The return value
-		/// is usually a subclass of the <see cref="IWebResponse"/> interface.
+		/// This constructor sets up the factory without any custom content type handlers. 
+		/// It uses the default behavior for creating instances of <see cref="NScrape.Interfaces.IWebResponse"/>.
 		/// </remarks>
-		public static Dictionary<string, Func<HttpWebResponse, IWebResponse>> SupportedContentTypes { get; }
-
-		/// <summary>
-		/// Creates an <see cref="IHtmlWebResponse"/>.
-		/// </summary>
-		/// <param name="httpWebResponse">
-		/// The original <see cref="HttpWebResponse"/>.
-		/// </param>
-		/// <returns>
-		/// A new <see cref="IHtmlWebResponse"/>.
-		/// </returns>
-		public static IWebResponse CreateHtmlResponse( HttpWebResponse httpWebResponse ) {
-			return new HtmlWebResponse( true, httpWebResponse );
+		public WebResponseFactory() : this( [] ) { 
 		}
 
 		/// <summary>
-		/// Creates a <see cref="IPlainTextWebResponse"/>.
+		/// Initializes a new instance of the <see cref="WebResponseFactory"/> class with the specified custom content type handlers.
 		/// </summary>
-		/// <param name="httpWebResponse">
-		/// The original <see cref="HttpWebResponse"/>.
+		/// <param name="customHandlers">
+		/// A collection of custom content type handlers to be used. These handlers will override the default handlers if they share the same content type prefix.
 		/// </param>
-		/// <returns>
-		/// A new <see cref="IPlainTextWebResponse"/>.
-		/// </returns>
-		public static IWebResponse CreatePlainTextResponse( HttpWebResponse httpWebResponse ) {
-			return new PlainTextWebResponse( true, httpWebResponse );
+		public WebResponseFactory( IEnumerable<IContentTypeHandler> customHandlers ) {
+			// Add our default handlers
+			handlers = defaultHandlers.ToDictionary( h => h.ContentTypePrefix, h => h, StringComparer.OrdinalIgnoreCase );
+
+			// Add the user's custom handlers, replacing ours if so specified
+			foreach ( var handler in customHandlers ) {
+				handlers[handler.ContentTypePrefix] = handler;
+			}
 		}
 
 		/// <summary>
-		/// Creates an <see cref="IXmlWebResponse"/>.
+		/// Creates an instance of <see cref="IWebResponse"/> based on the provided <see cref="HttpWebResponse"/>.
 		/// </summary>
-		/// <param name="httpWebResponse">
-		/// The original <see cref="HttpWebResponse"/>.
-		/// </param>
+		/// <param name="httpWebResponse">The HTTP web response to be wrapped in an <see cref="IWebResponse"/> instance.</param>
 		/// <returns>
-		/// A new <see cref="IXmlWebResponse"/>.
+		/// An implementation of <see cref="IWebResponse"/> that corresponds to the specified <paramref name="httpWebResponse"/>.
 		/// </returns>
-		public static IWebResponse CreateXmlResponse( HttpWebResponse httpWebResponse ) {
-			return new XmlWebResponse( true, httpWebResponse );
-		}
-
-		/// <summary>
-		/// Creates a <see cref="IBinaryWebResponse"/>.
-		/// </summary>
-		/// <param name="httpWebResponse">
-		/// The original <see cref="HttpWebResponse"/>.
-		/// </param>
-		/// <returns>
-		/// A new <see cref="IBinaryWebResponse"/>.
-		/// </returns>
-		public static IWebResponse CreateBinaryResponse( HttpWebResponse httpWebResponse ) {
-			return new BinaryWebResponse( true, httpWebResponse );
-		}
-
-		/// <summary>
-		/// Creates a <see cref="IJavaScriptWebResponse"/>.
-		/// </summary>
-		/// <param name="httpWebResponse">
-		/// The original <see cref="HttpWebResponse"/>.
-		/// </param>
-		/// <returns>
-		/// A new <see cref="IJavaScriptWebResponse"/>.
-		/// </returns>
-		public static IWebResponse CreateJavaScriptResponse( HttpWebResponse httpWebResponse ) {
-			return new JavaScriptWebResponse( true, httpWebResponse );
-		}
-
-		/// <summary>
-		/// Creates a <see cref="IJsonWebResponse"/>.
-		/// </summary>
-		/// <param name="httpWebResponse">
-		/// The original <see cref="HttpWebResponse"/>.
-		/// </param>
-		/// <returns>
-		/// A new <see cref="IJsonWebResponse"/>.
-		/// </returns>
-		public static IWebResponse CreateJsonResponse( HttpWebResponse httpWebResponse ) {
-			return new JsonWebResponse( true, httpWebResponse );
-		}
-
-		/// <summary>
-		/// Creates a <see cref="IWebResponse"/> for an <see cref="HttpWebResponse"/>, based on its
-		/// content type.
-		/// </summary>
-		/// <param name="httpWebResponse">
-		/// The <see cref="HttpWebResponse"/> to parse.
-		/// </param>
-		/// <returns>
-		/// If the content type is registered with the <see cref="WebResponseFactory"/>, the corresponding
-		/// <see cref="IWebResponse"/> object; otherwise, an <see cref="UnsupportedWebResponse"/> object.
-		/// </returns>
-		public static IWebResponse CreateResponse( HttpWebResponse httpWebResponse ) {
+		public IWebResponse CreateResponse( HttpWebResponse httpWebResponse ) {
 			if ( !httpWebResponse.Headers.AllKeys.Contains( CommonHeaders.ContentType ) ) {
 				// The response is missing a content type.
 				return new UnsupportedWebResponse( httpWebResponse.ResponseUri, string.Empty );
@@ -157,16 +96,16 @@ namespace NScrape {
 
 			var contentType = httpWebResponse.Headers[CommonHeaders.ContentType];
 
-			// The keys in the SupportedContentTypes indicate the text the contentType
-			// must start with. Find that key
-			var key = SupportedContentTypes.Keys.SingleOrDefault( k => contentType.StartsWith( k, StringComparison.OrdinalIgnoreCase ) );
-
-			// We don't support this content type
-			if ( key == null ) {
-				return new UnsupportedWebResponse( httpWebResponse.ResponseUri, contentType );
+			if ( contentType is null ) {
+				// The content type header is present but has no value.
+				return new UnsupportedWebResponse( httpWebResponse.ResponseUri, string.Empty );
 			}
 
-			return SupportedContentTypes[key]( httpWebResponse );
+			var key = handlers.Keys.SingleOrDefault( k => contentType.StartsWith( k, StringComparison.OrdinalIgnoreCase ) );
+
+			return key != null
+				? handlers[key].CreateResponse( httpWebResponse )
+				: new UnsupportedWebResponse( httpWebResponse.ResponseUri, contentType );
 		}
 	}
 }
